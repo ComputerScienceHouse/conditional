83824cfe2038ba6a82c678680a1bf0d4d8aba1b7
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-02-07 04:33:30 -0500

    moved around static files

--- conditional/__main__.py
+++ conditional/__main__.py
@@ -1,8 +1,30 @@
 from flask import Flask
 from flask import jsonify
 
+from dashboard import dashboard_bp
+
+import os
+
 import json
 import sys
+
+base_dir = os.getcwd()
+
+
+def app_path(*args):
+    return os.path.join(base_dir, *args)
+
+app = Flask(__name__)
+
+app.templates_folder = app_path("templates")
+
+app.register_blueprint(dashboard_bp)
+
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
 
 def web_main():
     json_config = None
@@ -10,7 +32,6 @@ def web_main():
     with open(sys.argv[1]) as config_file:
         json_config = json.load(config_file)
 
-    app = Flask(__name__)
     app.run(**json_config['flask'])
 
 if __name__ == '__main__':
---
+++ conditional/dashboard.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+from flask import render_template
+from flask import request
+
+dashboard_bp = Blueprint('dashboard_bp', __name__)
+
+@dashboard_bp.route('/dashboard')
+@dashboard_bp.route('/dashboard/')
+def display_dashboard():
+    # get user data
+
+    user_name = request.headers.get('x-webauth-user')
+
+    return render_template('index.html',
+                           username = user_name)
