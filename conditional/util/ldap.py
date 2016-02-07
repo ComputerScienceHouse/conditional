d96ef17c36a3c0d8b708855e715c40fed80e7f9d
James Forcier <csssuf@csssuf.net>
2016-02-07 04:07:15 -0500

    Add ldap support for handling group membership

--- conditional/util/ldap.py
+++ conditional/util/ldap.py
@@ -3,15 +3,19 @@ import ldap
 
 # Global state is gross. I'm sorry.
 ldap_conn = None
-search_ou = None
+user_search_ou = None
+group_search_ou = None
 
 class HousingLDAPError(Exception):
     pass
 
-def ldap_init(ldap_url, bind_dn, bind_pw, base_ou):
-    search_ou = base_ou
-    if search_ou == "" or search_ou is None:
-        raise HousingLDAPError("No search OU provided. Check your configs.")
+def ldap_init(ldap_url, bind_dn, bind_pw, user_ou, group_ou):
+    user_search_ou = user_ou
+    group_search_ou = group_ou
+    if user_search_ou == "" or user_search_ou is None:
+        raise HousingLDAPError("No user search OU provided.")
+    if group_search_ou == "" or group_search_ou is None:
+        raise HousingLDAPError("No group search OU provided.")
     ldap_conn = ldap.initialize(ldap_url, bytes_mode=False)
     ldap_conn.simple_bind_s(bind_dn, bind_pw)
 
@@ -25,14 +29,28 @@ def ldap_init_required(func):
 
 @ldap_init_required
 def _ldap_get_field(username, field):
-    ldap_results = ldap_conn.search_s(search_ou, ldap.SCOPE_SUBTREE, "(uid=%s)"
+    ldap_results = ldap_conn.search_s(user_search_ou, ldap.SCOPE_SUBTREE, "(uid=%s)"
             % username)
     if len(ldap_results) != 1:
-        raise HousingLDAPError("Wrong number of results found for username %s.")
+        raise HousingLDAPError("Wrong number of results found for username %s."
+                % username)
     return ldap_results[0][1][field]
+
+@ldap_init_required
+def _ldap_is_member_of_group(username, group):
+    ldap_results = ldap_conn.search_s(group_search_ou, ldap.SCOPE_SUBTREE,
+            "(cn=%s)" % group)
+    if len(ldap_results) != 1:
+        raise HousingLDAPError("Wrong number of results found for group %s." %
+                group)
+    return "uid=" + username + ",ou=Users,dc=csh,dc=rit,dc=edu" in \
+        [x.decode('ascii') for x in ldap_results[0][1]['member']]
 
 def ldap_get_housing_points(username):
     return int(_ldap_get_field('housingPoints'))
 
 def ldap_get_room_number(username):
     return _ldap_get_field('roomNumber')
+
+def ldap_is_eboard(username):
+    return _ldap_is_member_of_group(username, 'eboard')
