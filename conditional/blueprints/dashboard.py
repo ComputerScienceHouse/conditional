302036f57548ad1ded928f2523c5ef63c44d507d
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-02-08 18:03:15 -0500

    Finish skeleton for dashboard data and fix template

--- conditional/blueprints/dashboard.py
+++ conditional/blueprints/dashboard.py
@@ -13,12 +13,64 @@ def display_dashboard():
 
     data = {}
     data['username'] = user_name
+    # Member Status
+    data['active'] = True
+    # On-Floor Status
+    data['onfloor'] = True
+    # Voting Status
+    data['voting'] = False
 
+    freshman = {}
+
+    freshman['status'] = "Pending"
     # number of committee meetings attended
-    data['committee_meetings'] = 11
+    freshman['committee_meetings'] = 11
 
     # technical seminar total
-    data['ts_total'] = 2
+    freshman['ts_total'] = 42
+    freshman['ts_string'] = "Seminar #1\nSeminar #2"
 
-    #
+    freshman['hm_missed'] = 0
+
+    freshman['social_events'] = "Welcome Back, First Marks"
+
+    freshman['general_comments'] = "Please accept me as a member kthnxbai."
+
+    freshman['eval_date'] = "Oct 31, 2015"
+
+    data['freshman'] = freshman
+
+    spring = {}
+    spring['mp_status'] = "Failed"
+    spring['committee_meetings'] = 26
+    spring['hm_missed'] = 26
+    spring['general_comments'] = "I should win, please don't kick me out"
+
+    data['spring'] = spring
+    housing = {}
+    housing['points'] = 2
+    housing['room'] = "NRH3103"
+    housing['future_room'] = "NRH3102"
+    housing['queue_pos'] = 2
+    housing['queue_len'] = 9
+
+    data['housing'] = housing
+
+    major_projects = []
+    proj = {}
+    proj['name'] = "open container"
+    proj['status'] = "Passed"
+    proj['description'] = "Base project description example."
+    major_projects.append(proj)
+
+    data['major_projects'] = major_projects
+    data['major_projects_count'] = len(major_projects)
+
+    conditionals = [{'username':'loothelion','description':'redo freshman project','deadline':'next year', 'status': 'Failed'}]
+    data['conditionals'] = conditionals
+    data['conditionals_len'] = len(conditionals)
+
+    attendance = [{'type':'House Meeting', 'datetime': 'christmass'}]
+    data['attendance'] = attendance
+    data['attendance_len'] = len(attendance)
     return render_template('dashboard.html', **data)
