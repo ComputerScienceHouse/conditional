4f198eb94a2a2e209d4cd2354105e7bb6d5ed666
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-02-10 01:04:10 -0500

    Add introductory evals list with route fake content

--- conditional/blueprints/intro_evals.py
+++ conditional/blueprints/intro_evals.py
@@ -9,9 +9,26 @@ intro_evals_bp = Blueprint('intro_evals_bp', __name__)
 def display_intro_evals():
     # get user data
 
-    user_name = request.headers.get('x-webauth-user')
+    members = [
+                {
+                    'name': "Liam Middlebrook",
+                    'packet_due': '2015-12-23',
+                    'eval_date': '2016-02-13',
+                    'signatures_missed': 3,
+                    'committee_meetings': 24,
+                    'committee_meetings_passed': False,
+                    'house_meetings_missed': 0,
+                    'house_meetings_comments': "",
+                    'technical_seminars': "Seminar 1\nSeminar 2",
+                    'techincal_seminars_passed': True,
+                    'social_events': "",
+                    'freshmen_project': False,
+                    'comments': "please don't fail me",
+                    'result': 'Pending'
+                }
+            ]
 
     # return names in 'first last (username)' format
     return render_template('intro_evals.html',
-                           username = user_name)
+                           members = members)
 
