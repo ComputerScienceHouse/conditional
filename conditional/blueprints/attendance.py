f41555804891de6f42f9ed526294958ed1dc19f7
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-02-20 04:27:40 -0500

    Add seminar attendance route

--- conditional/blueprints/attendance.py
+++ conditional/blueprints/attendance.py
@@ -3,12 +3,23 @@ from flask import jsonify
 from flask import render_template
 from flask import request
 
-from util.ldap import ldap_get_non_alumni_members, ldap_get_name
+from util.ldap import ldap_get_all_members, ldap_get_non_alumni_members, ldap_get_name
 
 from db.models import CurrentCoops
 
 attendance_bp = Blueprint('attendance_bp', __name__)
 
+@attendance_bp.route('/attendance/ts_members')
+def get_all_members():
+    members = ldap_get_all_members()
+
+    named_members = []
+    for m in members:
+        uid = m
+        name = ldap_get_name(m)
+        named_members.append("{name} ({uid})".format(name=name, uid=uid))
+
+    return jsonify({'members': named_members}), 200
 
 @attendance_bp.route('/attendance/hm_members')
 def get_non_alumni_non_coop():
