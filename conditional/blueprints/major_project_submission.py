2a07e39de5e37c2240b31ae51806ecb773a5599a
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-02-20 04:24:39 -0500

    Add major project listing

--- conditional/blueprints/major_project_submission.py
+++ conditional/blueprints/major_project_submission.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import render_template
 from flask import request
 
+from db.models import MajorProject
 major_project_bp = Blueprint('major_project_bp', __name__)
 
 @major_project_bp.route('/major_project/')
@@ -10,6 +11,18 @@ def display_major_project():
 
     user_name = request.headers.get('x-webauth-user')
 
+    major_projects = [
+            {
+                'username': p.uid,
+                'name': p.name,
+                'status': p.status,
+                'description': p.description
+            } for p in
+        MajorProject.query.filter(MajorProject.uid == user_name)]
+
+    major_projects_len = len(major_projects)
     # return names in 'first last (username)' format
     return render_template('major_project_submission.html',
-                           username = user_name)
+                            major_projects = major_projects,
+                            major_projects_len = major_projects_len,
+                            username = user_name)
