423cb7c797fa01a1ca453e38d5247ac106108201
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-03-20 09:10:40 -0700

    Add freshman eval migration

--- conditional/db/migrate.py
+++ conditional/db/migrate.py
@@ -43,6 +43,13 @@ def idToCommittee(id):
         'Chairman'
         ]
     return committees[id]
+
+def getFid(name):
+    from db.models import FreshmanAccount
+
+    print(name)
+    return FreshmanAccount.query.filter(FreshmanAccount.name == name).first().id
+
 # Begin the Great Migration!
 def migrate_models():
 
@@ -50,9 +57,46 @@ def migrate_models():
     import db.models as models
 
     from db.database import db_session
-    #members = [m.username for m in zoo_session.query(zoo.Member).all()]
-    #print(members)
 
+    print("BEGIN: freshman evals")
+    # ==========
+
+    freshman_evals = [
+        {
+            'username': f.username,
+            'evalDate': f.voteDate,
+            'projectStatus': f.freshProjPass,
+            'signaturesMissed': f.numMissedSigs,
+            'socialEvents': f.socEvents,
+            'comments': f.comments
+        } for f in zoo_session.query(zoo.FreshmanEval).all()]
+
+    for f in freshman_evals:
+        if not f['username'].startswith('f_'):
+            # freshman who have completed packet and have a CSH account
+            eval = models.FreshmanEvalData(f['username'], f['signaturesMissed'])
+
+            # FIXME: Zookeeper was only pass/fail for freshman project not pending
+            if f['projectStatus'] == 1:
+                eval.freshman_project = 'Passed'
+
+            eval.social_events = f['socialEvents']
+            eval.other_notes = f['comments']
+
+            db_session.add(eval)
+        else:
+            # freshman not yet done with packet
+            account = models.FreshmanAccount(f['username'])
+            account.eval_date = f['evalDate']
+            db_session.add(account)
+
+    db_session.flush()
+
+    print("END: freshman evals")
+    # ==========
+
+    print("BEGIN: migrate committee meeting attendance")
+    # ==========
     c_meetings = [
         (
             m.meeting_date,
@@ -82,13 +126,27 @@ def migrate_models():
                 )
         ) for m in zoo_session.query(zoo.Attendance).all()]
 
-    print(com_meetings)
     for cm in c_meetings:
         if cm[1][1] == 8:
             continue
-        m = models.MemberCommitteeAttendance(cm[0], com_meetings.index(cm[1]) + 1)
-        db_session.add(m)
+        if cm[0].startswith('f_'):
+            f = models.FreshmanCommitteeAttendance(
+                getFid(cm[0]),
+                com_meetings.index(cm[1])
+            )
+            db_session.add(f)
+        else:
+            m = models.MemberCommitteeAttendance(cm[0], com_meetings.index(cm[1]) + 1)
+            db_session.add(m)
 
     db_session.flush()
+
+    print("END: migrate committee meeting attendance")
+    # ==========
+
+    print("BEGIN: migrate conditionals")
+    # ==========
+    # TODO implements db for conditionals
+    print("END: migrate conditionals")
+    db_session.flush()
     db_session.commit()
-    print(len(c_meetings))
