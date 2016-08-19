110a875452f0d91ac146a7c72311332b9b4e2689
Steven Mirabito <steven@stevenmirabito.com>
2016-08-19 13:24:02 -0400

    Fix production pipeline

--- gulpfile.js/tasks/rev/update-html.js
+++ gulpfile.js/tasks/rev/update-html.js
@@ -6,7 +6,7 @@ var path = require('path');
 // 5) Update asset references in HTML
 gulp.task('update-html', function () {
     var manifest = gulp.src(path.join(config.root.dest, "/rev-manifest.json"));
-    return gulp.src(path.join(config.root.dest, config.tasks.html.dest, '/**/*.html'))
+    return gulp.src(path.join(config.root.templates, '**/*.html'))
         .pipe(revReplace({manifest: manifest}))
-        .pipe(gulp.dest(path.join(config.root.dest, config.tasks.html.dest)))
+        .pipe(gulp.dest(path.join(config.root.templates)))
 });
