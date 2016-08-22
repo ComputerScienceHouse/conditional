0f66e85c4d583dca9f4f41b2023a28b2ae2a268f
Marc Billow <mbillow@me.com>
2016-08-21 21:32:45 -0400

    Actually fix tables

--- frontend/javascript/modules/table.js
+++ frontend/javascript/modules/table.js
@@ -7,30 +7,28 @@ export default class Table {
     this.table = table;
 
     // Set options based on data attributes
-    this.paginated = !(this.table.dataset.paginated === 'false');
     this.sortColumn = (_.isNil(this.table.dataset.sortColumn) ||
                         _.isNaN(this.table.dataset.sortColumn)) ?
                         1 : this.table.dataset.sortColumn;
     this.sortOrder = this.table.dataset.sortOrder === "asc" ? "asc" : "desc";
-    this.lengthChangable = this.table.dataset.lengthChangable === 'true';
+
+    this.options = {
+      lengthChange: this.table.dataset.lengthChangable === 'true',
+      info: false,
+      paging: !(this.table.dataset.paginated === 'false'),
+      pagingType: "numbers"
+    };
 
     // Just remove the search input from the DOM instead of disabling it
-    this.domOptions = "lrtip";
-    if (this.table.dataset.searchable === 'true') {
-      this.domOptions = "lfrtip";
+    if (this.table.dataset.searchable !== 'true') {
+      this.options.dom = "lrtip";
     }
 
     this.render();
   }
 
   render() {
-    this.table = $(this.table).DataTable({ // eslint-disable-line new-cap
-      // dom: this.domOptions,
-      lengthChange: this.lengthChangable,
-      info: false,
-      paging: this.paginated,
-      pagingType: "numbers"
-    })
+    this.table = $(this.table).DataTable(this.options) // eslint-disable-line new-cap
       .order([this.sortColumn, this.sortOrder])
       .draw();
   }
