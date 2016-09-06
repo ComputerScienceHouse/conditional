112c600b6f4966ce1b4d114d17a57022a88b33e2
Marc Billow <mbillow@me.com>
2016-09-06 17:40:52 -0400

    Whoops, forgot that was a field in the form...

--- conditional/blueprints/intro_evals.py
+++ conditional/blueprints/intro_evals.py
@@ -87,6 +87,7 @@ def display_intro_evals(internal=False):
                  ],
             'social_events': '',
             'freshman_project': "Pending",
+            'comments': "",
             'ldap_account': False,
             'status': "Pending"
         }
@@ -137,6 +138,7 @@ def display_intro_evals(internal=False):
                  ],
             'social_events': freshman_data.social_events,
             'freshman_project': freshman_data.freshman_project,
+            'comments': freshman_data.other_notes,
             'ldap_account': True,
             'status': freshman_data.freshman_eval_result
         }
