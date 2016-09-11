a38ecedf8e99acc276ba7f1eac39bb15d14ad1f3
Steven Mirabito <steven@stevenmirabito.com>
2016-09-10 22:49:15 -0400

    Add JS for deleting major projects; fixed some strings and typos.

--- frontend/javascript/modules/majorProjectStatus.js
+++ frontend/javascript/modules/majorProjectStatus.js
@@ -6,68 +6,95 @@ import FetchUtil from "../utils/fetchUtil";
 import sweetAlert from "../../../node_modules/bootstrap-sweetalert/dev/sweetalert.es6.js"; // eslint-disable-line max-len
 
 export default class MajorProjectStatus {
-  constructor(dropdown) {
-    this.dropdown = dropdown;
-    this.id = this.dropdown.dataset.id;
+  constructor(control) {
+    this.control = control;
+    this.id = this.control.dataset.id;
     this.endpoint = '/major_project/review';
+    this.deleteEndpoint = '/major_project/delete/';
     this.render();
   }
 
   render() {
-    const options = this.dropdown.querySelectorAll('[data-option]');
-    options.forEach(option => {
-      option.addEventListener('click', e => this._changeStatus(e));
-    });
+    if (this.control.tagName.toLowerCase() === "div") {
+      // Evals director dropdown
+      const options = this.control.querySelectorAll('[data-option]');
+      options.forEach(option => {
+        option.addEventListener('click', e => {
+          e.preventDefault();
+          this._changeStatus(e.target.dataset.option);
+        });
+      });
+    } else {
+      // Member self-delete button
+      this.control.addEventListener('click',
+          () => this._changeStatus('Delete'));
+    }
   }
 
-  _changeStatus(e) {
-    e.preventDefault();
-
-    let option = e.target.dataset.option;
-    let payload = {
-      id: this.id,
-      status: option
-    };
-
-    fetch(this.endpoint, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      credentials: "same-origin",
-      body: JSON.stringify(payload)
-    })
-      .then(FetchUtil.checkStatus)
-      .then(FetchUtil.parseJSON)
-      .then(response => {
-        if (response.hasOwnProperty('success') && response.success === true) {
-          let toggle = this.dropdown.querySelector('.dropdown-toggle');
-          ["btn-success", "btn-danger", "btn-warning"].forEach(classToRemove =>
-            toggle.classList.remove(classToRemove));
-
-          const caret = document.createElement('span');
-          caret.classList.add('caret');
-          toggle.text = option + " ";
-          toggle.appendChild(caret);
-
-          if (option === "Passed") {
-            toggle.classList.add("btn-success");
-          } else if (option === "Failed") {
-            toggle.classList.add("btn-danger");
-          } else {
-            toggle.classList.add("btn-warning");
-          }
+  _changeStatus(option) {
+    if (option === "Delete") {
+      FetchUtil.fetchWithWarning(this.deleteEndpoint + this.id, {
+        method: 'DELETE',
+        warningText: 'This action cannot be undone.',
+        successText: 'Major project deleted.'
+      }, () => {
+        let dashboardContainer = this.control.closest(".mp-container");
+        if (dashboardContainer) {
+          // Dashboard button
+          $(dashboardContainer).hide();
         } else {
-          sweetAlert("Uh oh...", "We're having trouble updating this project " +
-            "right now. Please try again later.", "error");
-          throw new Exception(FetchException.REQUEST_FAILED, response);
+          // Major projects page button
+          $(this.control.closest(".panel")).fadeOut();
         }
-      })
-      .catch(error => {
-        sweetAlert("Uh oh...", "We're having trouble updating this project " +
-          "right now. Please try again later.", "error");
-        throw new Exception(FetchException.REQUEST_FAILED, error);
       });
+    } else {
+      let payload = {
+        id: this.id,
+        status: option
+      };
+
+      fetch(this.endpoint, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        credentials: "same-origin",
+        body: JSON.stringify(payload)
+      })
+          .then(FetchUtil.checkStatus)
+          .then(FetchUtil.parseJSON)
+          .then(response => {
+            if (response.hasOwnProperty('success') &&
+                response.success === true) {
+              let toggle = this.control.querySelector('.dropdown-toggle');
+              ["btn-success", "btn-danger", "btn-warning"]
+                  .forEach(classToRemove =>
+                  toggle.classList.remove(classToRemove));
+
+              const caret = document.createElement('span');
+              caret.classList.add('caret');
+              toggle.text = option + " ";
+              toggle.appendChild(caret);
+
+              if (option === "Passed") {
+                toggle.classList.add("btn-success");
+              } else if (option === "Failed") {
+                toggle.classList.add("btn-danger");
+              } else {
+                toggle.classList.add("btn-warning");
+              }
+            } else {
+              sweetAlert("Uh oh...", "We're having trouble updating " +
+                  "this project right now. Please try again later.", "error");
+              throw new Exception(FetchException.REQUEST_FAILED, response);
+            }
+          })
+          .catch(error => {
+            sweetAlert("Uh oh...", "We're having trouble updating " +
+                "this project right now. Please try again later.", "error");
+            throw new Exception(FetchException.REQUEST_FAILED, error);
+          });
+    }
   }
 }
