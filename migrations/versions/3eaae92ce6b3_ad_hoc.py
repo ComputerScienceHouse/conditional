bdb57dff61317321d4cdc85084eb5807e6a5acd7
Devin Matte <devinmatte@gmail.com>
2019-09-09 11:46:42 -0400

    Adding Ad-Hoc to model

---
+++ migrations/versions/3eaae92ce6b3_ad_hoc.py
@@ -0,0 +1,31 @@
+"""ad_hoc
+
+Revision ID: 3eaae92ce6b3
+Revises: 4ac8ff82410a
+Create Date: 2019-09-06 15:32:42.472975
+
+"""
+
+# revision identifiers, used by Alembic.
+from alembic.ddl import postgresql
+
+revision = '3eaae92ce6b3'
+down_revision = '4ac8ff82410a'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+def upgrade():
+    op.alter_column('committee_meetings', 'committee',
+               existing_type=postgresql.ENUM('Evaluations', 'History', 'Social', 'Opcomm',
+                            'R&D', 'House Improvements', 'Financial', 'Chairman', 'Ad-Hoc', name="committees_enum"),
+               nullable=False)
+
+
+def downgrade():
+    op.alter_column('committee_meetings', 'committee',
+               existing_type=postgresql.Enum('Evaluations', 'History', 'Social', 'Opcomm',
+                            'R&D', 'House Improvements', 'Financial', 'Chairman', name="committees_enum"),
+               nullable=False)
