fbfc95947a63277522a7b0dc79d85bdb727c739f
Mobmaker <45888585+Mobmaker55@users.noreply.github.com>
2024-09-05 13:46:24 -0400

    Dropzone.js

--- frontend/javascript/modules/dropzone.js
+++ frontend/javascript/modules/dropzone.js
@@ -1,18 +1,26 @@
 import Dropzone from "dropzone";
+
 Dropzone.autoDiscover = false;
 
 export default class DropzoneUpload {
-  constructor(element) {
-    this.element = element;
-    this.render();
-  }
+    constructor(element) {
+        this.element = element;
+        this.render();
+    }
 
-  render() {
-    const dz = new Dropzone(this.element); // eslint-disable-line new-cap
-    dz.on("complete", () => window.location.reload());
-  }
+    render() {
+        const dz = new Dropzone(this.element); // eslint-disable-line new-cap
+        dz.on("complete", () => window.location.reload());
+    }
 }
 
 Dropzone.options.uploadUser = {
-  acceptedFiles: ".csv"
+    acceptedFiles: ".csv"
 };
+
+Dropzone.options.projectFiles = {
+    // autoQueue:false,
+    parallelUploads: 2,
+    uploadMultiple: true,
+    acceptedFiles: "audio/*,photo/*,video/*,.pdf,.doc,.docx"
+}
