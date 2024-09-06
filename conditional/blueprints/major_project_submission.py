2f42078429abd723aa59c2ed6a1104578c846422
Mobmaker <45888585+Mobmaker55@users.noreply.github.com>
2024-09-06 10:53:15 -0400

    whoever designed this confuses me immensely

--- conditional/blueprints/major_project_submission.py
+++ conditional/blueprints/major_project_submission.py
@@ -72,7 +72,8 @@ def upload_major_project_files(user_dict=None):
     # Temporarily save files to a place, to be uploaded on submit
 
     for _, file in request.files.lists():
-        safe_name = secure_filename(file[0].filename)
+        file = file[0] # remove it from the list because this is not the best
+        safe_name = secure_filename(file.filename)
         filename = f"/tmp/{user_dict['username']}/{safe_name}"
 
         os.makedirs(os.path.dirname(filename), exist_ok=True)
