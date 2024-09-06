d6daa6452f4942408fd6b3695ed1ae8360039b46
Mobmaker <45888585+Mobmaker55@users.noreply.github.com>
2024-09-06 01:18:19 -0400

    debug 2

--- conditional/blueprints/major_project_submission.py
+++ conditional/blueprints/major_project_submission.py
@@ -71,6 +71,7 @@ def upload_major_project_files(user_dict=None):
 
     # Temporarily save files to a place, to be uploaded on submit
 
+    print(request.files.lists())
     for _, file in request.files.lists():
         safe_name = secure_filename(file.filename)
         filename = f"/tmp/{user_dict['username']}/{safe_name}"
