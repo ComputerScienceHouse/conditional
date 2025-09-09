55ea7f8369b3a0462ac37a746cab4b05bfbbf73f
Jeremy Smart <jeremy3141592@gmail.com>
2025-09-09 19:21:43 -0400

    packet glue but even more (#399)

--- conditional/util/auth.py
+++ conditional/util/auth.py
@@ -104,7 +104,7 @@ def frosh_auth():
 
 
 @app.route("/auth/csh")
-@auth.oidc_auth("csh")
+@auth.oidc_auth("default")
 def csh_login():
     session["provider"] = "csh"
     return redirect("/packet", code=301)
