aa01b79389b6349a569f19a0ddf218d5c5741fef
Cole Stowell <cole@stowell.pro>
2025-10-12 14:58:27 -0400

    Revert "packet glue but again (#397)"
    This reverts commit baf42dfb8f12d549671948dd5e58edc8107d6947.

--- config.env.py
+++ config.env.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.getcwd(), "package.json")) as package_file:
 # Flask config
 DEBUG = env.get("CONDITIONAL_DEBUG", "false").lower() == "true"
 HOST_NAME = env.get("CONDITIONAL_HOST_NAME", "conditional.csh.rit.edu")
-SERVER_NAME = env.get("CONDITIONAL_SERVER_NAME", "conditional.csh.rit.edu")
+SERVER_NAME = env.get('CONDITIONAL_SERVER_NAME', 'conditional.csh.rit.edu')
 APP_NAME = "conditional"
 IP = env.get("CONDITIONAL_IP", "0.0.0.0")
 PORT = env.get("CONDITIONAL_PORT", 6969)
@@ -22,9 +22,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # LDAP config
 LDAP_RO = env.get("CONDITIONAL_LDAP_RO", "true").lower() == "true"
-LDAP_BIND_DN = env.get(
-    "CONDITIONAL_LDAP_BIND_DN", "cn=conditional,ou=Apps,dc=csh,dc=rit,dc=edu"
-)
+LDAP_BIND_DN = env.get("CONDITIONAL_LDAP_BIND_DN", "cn=conditional,ou=Apps,dc=csh,dc=rit,dc=edu")
 LDAP_BIND_PW = env.get("CONDITIONAL_LDAP_BIND_PW", "")
 
 # S3 information
@@ -36,28 +34,21 @@ AWS_SECRET_ACCESS_KEY = env.get("AWS_SECRET_ACCESS_KEY", "")
 # SENTRY_ENV is 'local-development'
 SENTRY_DSN = env.get("CONDITIONAL_SENTRY_DSN", "")
 SENTRY_CONFIG = {
-    "dsn": env.get("CONDITIONAL_SENTRY_LEGACY_DSN", ""),
-    "release": VERSION,
+    'dsn': env.get("CONDITIONAL_SENTRY_LEGACY_DSN", ""),
+    'release': VERSION,
 }
 SENTRY_ENV = env.get("CONDITIONAL_SENTRY_ENV", "local-development")
 
 # OIDC Config
-OIDC_ISSUER = env.get(
-    "CONDITIONAL_OIDC_ISSUER", "https://sso.csh.rit.edu/auth/realms/csh"
-)
+OIDC_ISSUER = env.get("CONDITIONAL_OIDC_ISSUER", "https://sso.csh.rit.edu/auth/realms/csh")
 OIDC_CLIENT_CONFIG = {
-    "client_id": env.get("CONDITIONAL_OIDC_CLIENT_ID", "conditional"),
-    "client_secret": env.get("CONDITIONAL_OIDC_CLIENT_SECRET", ""),
-    "post_logout_redirect_uris": [
-        env.get("CONDITIONAL_OIDC_CLIENT_LOGOUT", "http://0.0.0.0:6969/logout")
-    ],
+    'client_id': env.get("CONDITIONAL_OIDC_CLIENT_ID", "conditional"),
+    'client_secret': env.get("CONDITIONAL_OIDC_CLIENT_SECRET", ""),
+    'post_logout_redirect_uris': [env.get("CONDITIONAL_OIDC_CLIENT_LOGOUT", "http://0.0.0.0:6969/logout")]
 }
-FROSH_OIDC_ISSUER = env.get(
-    "CONDITIONAL_FROSH_OIDC_ISSUER", "https://sso.csh.rit.edu/auth/realms/csh"
-)
 
 # Openshift secret
-SECRET_KEY = env.get("CONDITIONAL_SECRET_KEY", default="".join(secrets.token_hex(16)))
+SECRET_KEY = env.get("CONDITIONAL_SECRET_KEY", default=''.join(secrets.token_hex(16)))
 
 # General config
 DUES_PER_SEMESTER = env.get("CONDITIONAL_DUES_PER_SEMESTER", 80)
