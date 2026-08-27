98a0e53db4af5068ab1410ed626e41af5ba02850
Noah Hanford (spaced) <spaced@csh.rit.edu>
2026-08-27 13:15:13 -0400

    fix: excusing people

--- frontend/javascript/modules/editUser.js
+++ frontend/javascript/modules/editUser.js
@@ -286,7 +286,7 @@ export default class EditUser {
       });
     } else {
       // Save missed house meetings
-      let missedHms = modal.querySelectorAll('.hm-wrapper');
+      let missedHms = modal.querySelectorAll('.missed-hm-tpl');
       missedHms.forEach(hm => {
         let payload = {
           id: hm.dataset.id,
